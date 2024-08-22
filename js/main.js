6ab3e65ed0a46c5e49716614f51e1dd16aab65ba
AdrianaMariaCaro <162487165+AdrianaMariaCaro@users.noreply.github.com>
2024-08-22 13:05:23 +0200

    2 pre entrega

--- js/main.js
+++ js/main.js
@@ -1,29 +1,60 @@
-let ingrediente1 = prompt ("Elige que queres cenar \n 1 Hamburguesa \n2 Pizza \n3 Pasta");
+alert("Bienvenido, a continuación elija los ingredientes de su combo")
 
-function cena(){
-    switch (ingrediente1) {
-        case "1":
-           console.log  ("Usted a eligido Hamburguesa para cenar")
-            break
-        case "2":
-            console.log ("Usted a eligido Pizza para cenar")
-            break
-            case "3":
-                console.log ("Usted a eligido Pasta para cenar")
-            break
-        default:
-            console.log ("Lo siento, solo hay para cenar hamburguesa, pizza o pasta")
-    }
-}
+const comidaPrincipal = [
+    { nombre: "Pizza", precio: 4000},
+    {nombre: "Pasta", precio:3000},
+    {nombre: "Hamburguesa", precio: 5000}
+];
 
-cena()
+const guarnicion =[
+    {nombre: "Ensalda", precio: 2000},
+    {nombre: "Neguets", precio: 2500},
+    {nombre:"Papas Fritas", precio: 3000}
+];
 
-if (ingrediente1=="1"){
-    console.log ("el precio de la Hamburguesa son 10mil pesos");
-} else if (ingrediente1 =="2"){
-    console.log ("el precio de la Pizza son 7mil pesos");
-}else if (ingrediente1=="3"){
-    console.log("el precio de la pasta son 4mil pesos");
-} else{
-    console.log ("la opción elegida no es valida")
-}
+const bebidas =[
+    {nombre: "Agua", precio: 1500},
+    {nombre: "Gaseosa", precio: 2500},
+    {nombre:"Cerveza", precio: 3000}
+];
+
+
+
+
+let precioTotal=0;
+
+function seleccionarOpcion(arrayOpciones, mensaje) {
+    let opcionSeleccionada;
+    do {
+      opcionSeleccionada = parseInt(prompt(mensaje));
+    } while (
+      isNaN(opcionSeleccionada) ||
+      opcionSeleccionada < 1 ||
+      opcionSeleccionada > arrayOpciones.length
+    );
+    return arrayOpciones[opcionSeleccionada - 1];
+  }
+
+const hamburguesaSeleccionada = seleccionarOpcion(comidaPrincipal, "Elige que queres cenar \n 1 Pizza \n2 Pasta  \n3 Hamburguesa :");
+precioTotal += hamburguesaSeleccionada.precio;
+console.log(`Hamburguesa seleccionada: ${hamburguesaSeleccionada.nombre} (${hamburguesaSeleccionada.precio}$)`);
+
+const guarnicionSeleccionado = seleccionarOpcion(guarnicion, "Elige que una guarnición \n 1 Ensalada \n2 Nuguets \n3 Papas Fritas");
+precioTotal += guarnicionSeleccionado.precio;
+console.log(`Postre seleccionado: ${guarnicionSeleccionado.nombre} (${guarnicionSeleccionado.precio}$)`);
+
+const bebidaSeleccionada = seleccionarOpcion(bebidas, "Elige que queres de beber \n 1 Agua \n2 Gaseosa \n3 Cerveza");
+precioTotal += bebidaSeleccionada.precio;
+console.log(`Bebida seleccionada: ${bebidaSeleccionada.nombre} (${bebidaSeleccionada.precio}$)`);
+
+
+const postre =[
+  {nombre:"helado" , precio: 2000},
+  { nombre:"flan", precio: 1500},
+  {nombre: "torta", precio: 2300},
+]
+
+const postreElegido = prompt ("ingrese que desea de postre");
+console.log (postre.find ((postre) => postre.nombre === postreElegido))
+;
+alert(`Precio de su combro es de: ${precioTotal}, mas lo que vale su ${postreElegido}`);
